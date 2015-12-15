67fea85967ac99a9271a561502f0bc2107f86972
Daniel Sank <sank.daniel@gmail.com>
2015-12-15 00:25:20 -0500

    Fix up type variance in stack.

--- src/main/scala/stack.scala
+++ src/main/scala/stack.scala
@@ -1,18 +1,19 @@
 object StackMain {
-  abstract class Stack[A] {
-    def push(x: A): Stack[A] = new NonEmptyStack[A](x, this)
+  abstract class Stack[+A] {
+    def push[B >: A](x: B): Stack[B] = new NonEmptyStack[B](x, this)
     def isEmpty: Boolean
     def top: A
     def pop: Stack[A]
-    def isPrefix(other: Stack[A]): Boolean = {
-      def recur(a: Stack[A], b: Stack[A]): Boolean = {
+    def isPrefix[B >: A](other: Stack[B]): Boolean = {
+      def recur(a: Stack[A], b: Stack[B]): Boolean = {
+        // Is a a prefix of b?
         a.isEmpty ||
         a.top == b.top && recur(a.pop, b.pop)
       }
       recur(this, other)
     }
   }
-  class EmptyStack[A] extends Stack[A] {
+  object EmptyStack extends Stack[Nothing] {
     def isEmpty = true
     def top = error("EmptyStack.top")
     def pop = error("EmptyStack.pop")
@@ -23,8 +24,8 @@ object StackMain {
     def pop = rest
   }
   def main(args: Array[String]) = {
-    val myStack = new EmptyStack[Int]
-    val stack1 = myStack.push(4)
+    val myStack = EmptyStack.push(54)
+    val stack1 = myStack.push(6.6)
     val stack2 = myStack.push(3)
     println(stack2.top)
   }
