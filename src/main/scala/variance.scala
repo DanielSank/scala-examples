159df2299e1bc01405feb346a53545519208f571
Daniel Sank <sank.daniel@gmail.com>
2015-12-28 20:08:19 -0500

    Fix up variance example.

--- src/main/scala/variance.scala
+++ src/main/scala/variance.scala
@@ -1,14 +1,14 @@
 object VarianceMain {
-  // Take a look at the compiler errors here.
-  // Try fixing it by making the type parameter of Foo covariant
-  // and see what happens.
 
   class Foo[A] {
     def func(a: A) = {}
   }
 
   def main(args: Array[String]): Unit = {
-    val foo = new Foo[String]
-    val bar: Foo[Any] = foo
+    // Uncomment the next two lines, then take a look at the compiler errors.
+    // Try fixing it by making the type parameter of Foo covariant.
+
+    // val foo = new Foo[String]
+    // val bar: Foo[Any] = foo
   }
 }
