68cc6a8531d88c5e7dd20ef2a57d205d3b34c43d
Daniel Sank <sank.daniel@gmail.com>
2015-12-30 10:51:22 -0500

    Improve Stack class.

--- src/main/scala/stack.scala
+++ src/main/scala/stack.scala
@@ -1,35 +1,39 @@
+class Fruit
+class Apple extends Fruit
+class Gala extends Apple
+class Banana extends Fruit
+
 abstract class Stack[+A] {
-  def isEmpty: Boolean
+  def push[B >: A](x: B): Stack[B] = new NonEmptyStack[B](x, this)
   def top: A
   def pop: Stack[A]
   def isPrefix[B >: A](other: Stack[B]): Boolean = {
-    def recur(a: Stack[A], b: Stack[B]): Boolean = {
-      a.isEmpty ||
-      a.top == b.top && recur(a.pop, b.pop)
+    def recur(a: Stack[A], b: Stack[B]): Boolean = a match {
+      case EmptyStack => true
+      case NonEmptyStack(top, rest) => top == b.top && recur(a.pop, b.pop)
     }
     recur(this, other)
   }
 }
 
 case object EmptyStack extends Stack[Nothing] {
-  def push[A](x: A): NonEmptyStack[A] = new NonEmptyStack[A](x, this)
-  def isEmpty = true
   def top = error("EmptyStack.top")
   def pop = error("EmptyStack.pop")
 }
 
 case class NonEmptyStack[A](elem: A, rest: Stack[A]) extends Stack[A] {
-  def push[B <% A](x: B): Stack[A] = new NonEmptyStack[A](x, this)
-  def isEmpty = false
   def top = elem
   def pop = rest
 }
 
 object StackMain {
   def main(args: Array[String]) = {
-    val myStack = EmptyStack.push(54.6)
-    val x = 4
-    val stack1 = myStack.push(x)
-    println(stack1.top)
+    val apple = new Apple
+    val gala = new Gala
+    val banana = new Banana
+
+    val stack1 = EmptyStack.push(apple)
+    val stack2 = stack1.push(gala)
+    val stack3 = stack2.push(banana)
   }
 }
