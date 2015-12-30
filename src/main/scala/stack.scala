b8509d5a8a222f83d9d498faa32ca78dd6de28a0
Daniel Sank <sank.daniel@gmail.com>
2015-12-29 20:09:30 -0500

    Fix variance of Stack.push.

--- src/main/scala/stack.scala
+++ src/main/scala/stack.scala
@@ -1,32 +1,35 @@
-object StackMain {
-  abstract class Stack[+A] {
-    def push[B >: A](x: B): Stack[B] = new NonEmptyStack[B](x, this)
-    def isEmpty: Boolean
-    def top: A
-    def pop: Stack[A]
-    def isPrefix[B >: A](other: Stack[B]): Boolean = {
-      def recur(a: Stack[A], b: Stack[B]): Boolean = {
-        // Is a a prefix of b?
-        a.isEmpty ||
-        a.top == b.top && recur(a.pop, b.pop)
-      }
-      recur(this, other)
+abstract class Stack[+A] {
+  def isEmpty: Boolean
+  def top: A
+  def pop: Stack[A]
+  def isPrefix[B >: A](other: Stack[B]): Boolean = {
+    def recur(a: Stack[A], b: Stack[B]): Boolean = {
+      a.isEmpty ||
+      a.top == b.top && recur(a.pop, b.pop)
     }
+    recur(this, other)
   }
-  object EmptyStack extends Stack[Nothing] {
-    def isEmpty = true
-    def top = error("EmptyStack.top")
-    def pop = error("EmptyStack.pop")
-  }
-  class NonEmptyStack[A](elem: A, rest: Stack[A]) extends Stack[A] {
-    def isEmpty = false
-    def top = elem
-    def pop = rest
-  }
+}
+
+case object EmptyStack extends Stack[Nothing] {
+  def push[A](x: A): NonEmptyStack[A] = new NonEmptyStack[A](x, this)
+  def isEmpty = true
+  def top = error("EmptyStack.top")
+  def pop = error("EmptyStack.pop")
+}
+
+case class NonEmptyStack[A](elem: A, rest: Stack[A]) extends Stack[A] {
+  def push[B <% A](x: B): Stack[A] = new NonEmptyStack[A](x, this)
+  def isEmpty = false
+  def top = elem
+  def pop = rest
+}
+
+object StackMain {
   def main(args: Array[String]) = {
-    val myStack = EmptyStack.push(54)
-    val stack1 = myStack.push(6.6)
-    val stack2 = myStack.push(3)
-    println(stack2.top)
+    val myStack = EmptyStack.push(54.6)
+    val x = 4
+    val stack1 = myStack.push(x)
+    println(stack1.top)
   }
 }
