cb1cba63012fc7d44028b479cf534eadc767cb8e
Daniel Sank <sank.daniel@gmail.com>
2015-12-30 16:14:53 -0500

    Document and simplify calculator.

--- src/main/scala/calculator.scala
+++ src/main/scala/calculator.scala
@@ -1,20 +1,42 @@
 object CalculatorMain {
 
-  // Partial (curried)
+  // Evaluate a function over a range of integers and sum the results.
+  def sumFunction(f: Int => Int, a: Int, b: Int): Int = {
+    if (a > b) 0 else f(a) + sumFunction(f, a + 1, b)
+  }
+
+  /* Note that the above implementation is not tail recursive. Also not that it
+   * is not possible to partially evaluate the function. For example, suppose
+   * we have a particular mathematical function f: Int => Int which we would
+   * like to evaluate and sum over several different ranges of integers. To do
+   * that here, we'd have to do something like this
+   * 
+   * def square(x: Int): Int = {x*x}
+   * 
+   * def sumSquares(a: Int, b: Int): Int = {sumFunction(square, a, b)}
+   */
+
+  /* This version uses Currying to make partial evaluation easy.
+   * 
+   * For example, we can construct a function equivalent to the sumSquares
+   * described above with less typing:
+   * 
+   * sumSquares = sumFunction1(square)_
+   */
   def sumFunction1(f: Int => Int)(a: Int, b: Int): Int = {
     if (a > b) 0 else f(a) + sumFunction1(f)(a + 1, b)
   }
 
-  // Tail recursive
-  def sumFunction2(f: Int => Int): (Int, Int, Int) => Int = {
-    def sum(a: Int, b: Int, total: Int): Int = {
-      if (a <= b) sum(a+1, b, f(a) + total)
+  // This version is tail recursive.
+  def sumFunction2(f: Int => Int, a: Int, b: Int): Int = {
+    def sum(x: Int, y: Int, total: Int): Int = {
+      if (x <= y) sum(x + 1, y, f(x) + total)
       else total
     }
-    sum
+    sum(a, b, 0)
   }
 
-  // Tail recursive and partial (curried)
+  // This version is tail recursive and Curried.
   def sumFunction3(f: Int => Int)(a: Int, b: Int): Int = {
     def recur(total: Int)(f: Int => Int)(a: Int, b: Int): Int = {
         if (a <= b) recur(f(a) + total)(f)(a+1, b)
@@ -27,10 +49,12 @@ object CalculatorMain {
     def square(x: Int): Int = {x*x}
     println("Sum of squares from 5 to 8 = 174")
 
+    println(s"sumFunction: ${sumFunction(square, 5, 8)}")
+
     val sum1 = sumFunction1(square)_
     println(s"sumFunction1: ${sum1(5, 8)}")
 
-    println(s"sumFunction2: ${sumFunction2(square)(5, 8, 0)}")
+    println(s"sumFunction2: ${sumFunction2(square, 5, 8)}")
 
     val sum3 = sumFunction3(square)_
     println(s"sumFunction3: ${sum3(5, 8)}")
